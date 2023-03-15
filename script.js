53148d9bde9cede27ce4f93a0b3d810e0c62822e
David Simon <110782993+obnok@users.noreply.github.com>
2023-03-15 15:25:19 +0000

    added upgrades

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
+var interval = window.setInterval(callBack, 1000);
+var LPS = 0;
+
+function callBack() {
+    const counter = document.getElementById('countNum');
+    counter.innerHTML = parseInt(counter.innerHTML,10) + LPS;
+}
 
 function increment() {
     const counter = document.getElementById('countNum');
     counter.innerHTML = (parseInt(counter.innerHTML,10)+1);
+}
+
+function upgrade25() {
+    const counter = document.getElementById('countNum');
+    let num = parseInt(counter.innerHTML,10);
+    if (num >= 25) {
+        document.getElementById('upgradeMe').innerHTML = 'Upgraded Lee Clicker';
+        counter.innerHTML = num - 25;
+    }
+}
+
+function autoLee1() {
+    const counter = document.getElementById('countNum');
+    const oneSec = document.getElementById('oneSec');
+    let num = parseInt(counter.innerHTML,10);
+    if (num >= 50) {
+        LPS++;
+        counter.innerHTML = num - 50;
+        oneSec.innerHTML = parseInt(oneSec.innerHTML,10)+1;
+    }
 }
