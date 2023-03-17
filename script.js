5224824b4664a1fd5eeeac7004834770da71fcdb
David Simon <110782993+obnok@users.noreply.github.com>
2023-03-17 15:16:34 +0000

    handoff to lashay

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var interval = window.setInterval(callBack, 1000);
 var LPS = 0;
+let inc = 1;
 
 function callBack() {
     const counter = document.getElementById('countNum');
@@ -8,25 +9,27 @@ function callBack() {
 
 function increment() {
     const counter = document.getElementById('countNum');
-    counter.innerHTML = (parseInt(counter.innerHTML,10)+1);
+    counter.innerHTML = (parseInt(counter.innerHTML,10) + inc);
 }
 
-function upgrade25() {
+document.getElementById('upgradeClick').addEventListener("click", function() {
     const counter = document.getElementById('countNum');
+    const oneClick = document.getElementById('oneClick');
     let num = parseInt(counter.innerHTML,10);
     if (num >= 25) {
-        document.getElementById('upgradeMe').innerHTML = 'Upgraded Lee Clicker';
+        inc++;
         counter.innerHTML = num - 25;
+        oneClick.innerHTML = "Lee's Per Click: " + parseInt(oneClick.innerHTML,10)+1;
     }
-}
+});
 
-function autoLee1() {
+document.getElementById('upgradeAuto').addEventListener("click", function() {
     const counter = document.getElementById('countNum');
     const oneSec = document.getElementById('oneSec');
     let num = parseInt(counter.innerHTML,10);
     if (num >= 50) {
         LPS++;
         counter.innerHTML = num - 50;
-        oneSec.innerHTML = parseInt(oneSec.innerHTML,10)+1;
+        oneSec.innerHTML = "AutoLee's Per Second: " + parseInt(oneSec.innerHTML,10)+1;
     }
-}
+});
